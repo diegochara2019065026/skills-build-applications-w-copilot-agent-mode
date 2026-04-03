e12f78b53ea76f1df7f5e930af7f2479f5bbfd5a
diegochara2019065026 <106203631+diegochara2019065026@users.noreply.github.com>
2026-04-03 15:59:31 +0000

    Update Django app with Codespace URL endpoint

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = 'django-insecure-oi=ak_(z=#^yy$av#&r%e#eos43g)daxq+!f#r$-*qr%k3mc9a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
+ALLOWED_HOSTS.append('*')  # Para desarrollo, quitar en producción
 
 
 # Application definition
